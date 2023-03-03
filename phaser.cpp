1d191262fd28cbf1878da6547feb1f6a569c71b4
caronkey <guangzep@gmail.com>
2023-03-03 14:17:01 +0800

    fix ./1 error

--- phaser.cpp
+++ phaser.cpp
@@ -152,8 +152,8 @@ void Phaser::phasing()
         if (!contigs.empty() && (std::find(contigs.begin(), contigs.end(), frvcf->contigs[rid]) == contigs.end())) {
             std::string mess = " skipp " + std::string(frvcf->contigs[rid]);
             logging(std::cerr, mess);
-//            continue;
-            fwvcf->write_nxt_contigs(frvcf->contigs[rid].data(), chromo_phaser, *frvcf,spectral->getBreakIdxs());
+            continue;
+//            fwvcf->write_nxt_contigs(frvcf->contigs[rid].data(), chromo_phaser, *frvcf,spectral->getBreakIdxs());
 //            prev_variant_count += chromo_phaser->variant_count;
 //            spectral->release_chromo_phaser();
 //            delete chromo_phaser;
